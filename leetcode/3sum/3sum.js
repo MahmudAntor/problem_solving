99466d64afc5ced848e6cf9bc86544f315076da8
MahmudAntor <antor.mahmud96@gmail.com>
2022-03-19 12:45:10 +0600

    leetcode: 3Sum, Type: array, Solution: Time-O(n^2), Space-O(1)

---
+++ leetcode/3sum/3sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  nums.sort((a, b) => a - b);
+  let result = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (left < right && nums[left] === nums[left - 1]) left++;
+        while (left < right && nums[right] === nums[right + 1]) right--;
+      } else if (sum > 0) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(threeSum([0, -1, 0, 0, -1, 1]));
---
+++ leetcode/3sum/README.md
@@ -0,0 +1,9 @@
+Problem Link: https://leetcode.com/problems/3sum/
+
+Problem Type: Array
+
+Solution: using two pointer technique
+
+Complexity Analysis:
+	Time complexity : O(n^2)
+	Space complexity : O(1)
